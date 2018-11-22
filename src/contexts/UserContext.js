ac8d618d00e4be09f3a6feb4b56b790ff5c68df2
hellomac87 <hellomac87@gmail.com>
2018-11-22 15:23:05 +0900

    사용자 정보 캐시

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
+import api from '../api';
 
 const { Provider, Consumer } = React.createContext();
 
 class UserProvider extends Component {
     constructor(props){
         super(props);
+
+        // 로그인 함수
+        this.login = async (username, password) => {
+
+            const res = await api.post('/users/login', {
+                    username,
+                    password
+                })
+            localStorage.setItem('token', res.data.token);
+
+            await this.refreshUser();
+            // TODO: PostList 보여주기
+        }
+
+        
+
         this.state = {
             user: {
-                username: '이강산',
-            }
+                id: null,
+                username: null
+            },
+            login: this.login
         }
     }
+
+    refreshUser = async () => {
+        const { data } = await api.get('/me');
+        this.setState({
+            user: {
+                id: data.id,
+                username: data.username
+            }
+        });
+    }
+
+    async componentDidMount(){
+        if(localStorage.getItem('token')){
+            await this.refreshUser();
+        }
+    }
+
     render() {
         return (
             <Provider value={this.state}>
