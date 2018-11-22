a955707dffd5970946398f6b96b139d4a5893b9e
hellomac87 <hellomac87@gmail.com>
2018-11-22 19:35:21 +0900

    컴포넌트에서 context 를 props로 사용하기(Useconsumer LoginForm 에 둘러쌈)

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {UserConsumer} from '../contexts/UserContext';
 
-export default class LoginForm extends React.Component {
+class LoginForm extends React.Component {
   constructor(props) {
     super(props)
   
@@ -9,36 +9,37 @@ export default class LoginForm extends React.Component {
     this.passwordRef = React.createRef()
   }
   
-  async handleSubmit(e) {
-    
+  handleSubmit = async(e) => {
+    e.preventDefault();
+    const username = this.usernameRef.current.value
+    const password = e.target.elements.password.value
+    this.props.login(username, password);
     
   }
   render() {
     const {onRegister} = this.props
     return (
-        <UserConsumer>
-          {
-            ({login}) => {
-              return (
-                <React.Fragment>
-                  <form onSubmit={e => {
-                    e.preventDefault();
-                    const username = this.usernameRef.current.value
-                    const password = e.target.elements.password.value
-                    login(username, password);
-                  }}>
-                    <h1>로그인</h1>
-                    <input ref={this.usernameRef} type="text" name="username" />
-                    <input ref={this.passwordRef} type="password" name="password" />
+      <React.Fragment>
+        <form onSubmit={e => {
+          this.handleSubmit(e)
+        }}>
+          <h1>로그인</h1>
+          <input ref={this.usernameRef} type="text" name="username" />
+          <input ref={this.passwordRef} type="password" name="password" />
 
-                    <button>로그인</button>
-                  </form>
-                  <button onClick={() => onRegister()}>회원 가입</button>
-                </React.Fragment>
-              )
-            }
-          }
-        </UserConsumer>
+          <button>로그인</button>
+        </form>
+        <button onClick={() => onRegister()}>회원 가입</button>
+      </React.Fragment>
     )
   }
 }
+
+export default (props) => (
+  <UserConsumer>
+    {
+      ({login}) => <LoginForm {...props} login={login} />
+    }
+  </UserConsumer>
+)
+  
