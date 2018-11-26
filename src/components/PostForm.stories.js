fd3555f0ceabb01d5140d66b93148909692b42dd
hellomac87 <hellomac87@gmail.com>
2018-11-26 16:39:53 +0900

    storybook

---
+++ src/components/PostForm.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import PostForm from './PostForm';
+
+storiesOf('PostForm', module)
+  .add('default', () => <PostForm />)
+  .add('editing', () => <PostForm editing={true}/>);
