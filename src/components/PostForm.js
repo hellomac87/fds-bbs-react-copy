9fb735e682a69f3c7c49f34e3502a07677f7544e
hellomac87 <hellomac87@gmail.com>
2018-11-26 14:24:56 +0900

    prop을 통해 다른 스타일 넣어주기

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -6,11 +6,13 @@ import classNames from 'classnames';
 // defaultValue에 다른 값을 또 넣어주지 않도록 주의
 export default class PostForm extends Component {
   render() {
+    const { editing } = this.props;
+    const titleClass = classNames(styles.titleInput, { [styles.editing]: editing })
     return (
       <div>
         <form onSubmit={e => this.props.onSubmit(e)}>
           <input 
-            className={classNames(styles.titleInput)}
+            className={titleClass}
             type="text" name="title" defaultValue={this.props.title} 
           />
           <textarea name="body" cols="30" rows="10" defaultValue={this.props.body}></textarea>
